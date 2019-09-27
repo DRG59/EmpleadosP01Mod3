59c2e9606207fec48e6fbcba64da08970d87aedb
fotocopia <catalina.pimentel@uniacc.edu>
2019-09-27 14:57:34 -0300

    Version Original

---
+++ EmpleadosP01Mod3/src/java/cl/duoc/Empleado.java
@@ -0,0 +1,66 @@
+package cl.duoc;
+
+public class Empleado {
+
+    private String rut;
+    private String nombre;
+    private int edad;
+    private int antiguedad;
+
+    public Empleado() {
+    }
+
+    public Empleado(String rut, String nombre, int edad, int antiguedad) {
+        this.rut = rut;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.antiguedad = antiguedad;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getAntiguedad() {
+        return antiguedad;
+    }
+
+    public void setAntiguedad(int antiguedad) {
+        this.antiguedad = antiguedad;
+    }
+
+    public void Calculadora(int antiguedad) {
+        this.antiguedad = antiguedad;
+    }
+
+    public int montoBono() {        
+        int monto = 0;
+
+        if (this.antiguedad > 3) {
+            monto = this.antiguedad * 50000;
+        }
+        return monto;
+
+    }
+
+}
